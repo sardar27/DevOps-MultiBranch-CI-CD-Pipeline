3f8ac57a669e65b462b83bf070f6ef9545ae78cd
Mahammed Sardar <sardarmahammed5@gmail.com>
2023-03-05 08:48:58 +0530

    paker action no & terraform apply yes

--- ec2.tf
+++ ec2.tf
@@ -91,23 +91,23 @@ resource "aws_instance" "worker-2" {
   }
 }
 
-# resource "aws_instance" "worker-3" {
-#   #ami                         = data.aws_ami.my_ami.id
-#     ami = var.imagename
-#   availability_zone           = "us-east-1a"
-#   instance_type               = "t2.small"
-#   key_name                    = "jenkey"
-#   subnet_id                   = aws_subnet.subnet1-public.id
-#   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
-#   associate_public_ip_address = true
-#   tags = {
-#     Name       = "swarm-worker-3"
-#     Env        = "Prod"
-#     Owner      = "Sree"
-#     CostCenter = "ABCD"
-#   }
-# }
-
+ resource "aws_instance" "worker-3" {
+   #ami                         = data.aws_ami.my_ami.id
+     ami = var.imagename
+   availability_zone           = "us-east-1a"
+   instance_type               = "t2.small"
+   key_name                    = "jenkey"
+   subnet_id                   = aws_subnet.subnet1-public.id
+   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+   associate_public_ip_address = true
+   tags = {
+     Name       = "swarm-worker-3"
+     Env        = "Prod"
+     Owner      = "Sree"
+     CostCenter = "ABCD"
+   }
+ }
+ 
 # resource "aws_instance" "worker-4" {
 #   #ami                         = data.aws_ami.my_ami.id
 #     ami = var.imagename
