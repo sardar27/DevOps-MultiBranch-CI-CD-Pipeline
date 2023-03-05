a9c92d22615ca2e789e78644f4df3f4dbd44fecb
Mahammed Sardar <sardarmahammed5@gmail.com>
2023-03-05 08:41:13 +0530

    paker action no & terraform apply yes

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "master-1" {
   ami = var.imagename
   availability_zone           = "us-east-1a"
   instance_type               = "t2.small"
-  key_name                    = "newppk"
+  key_name                    = "jenkey"
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
@@ -27,7 +27,7 @@ resource "aws_instance" "master-2" {
     ami = var.imagename
   availability_zone           = "us-east-1a"
   instance_type               = "t2.small"
-  key_name                    = "newppk"
+  key_name                    = "jenkey"
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
@@ -44,7 +44,7 @@ resource "aws_instance" "master-3" {
     ami = var.imagename
   availability_zone           = "us-east-1a"
   instance_type               = "t2.small"
-  key_name                    = "newppk"
+  key_name                    = "jenkey"
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
@@ -62,7 +62,7 @@ resource "aws_instance" "worker-1" {
     ami = var.imagename
   availability_zone           = "us-east-1a"
   instance_type               = "t2.small"
-  key_name                    = "newppk"
+  key_name                    = "jenkey"
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
@@ -79,7 +79,7 @@ resource "aws_instance" "worker-2" {
   ami = var.imagename
   availability_zone           = "us-east-1a"
   instance_type               = "t2.small"
-  key_name                    = "newppk"
+  key_name                    = "jenkey"
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
@@ -96,7 +96,7 @@ resource "aws_instance" "worker-2" {
 #     ami = var.imagename
 #   availability_zone           = "us-east-1a"
 #   instance_type               = "t2.small"
-#   key_name                    = "newppk"
+#   key_name                    = "jenkey"
 #   subnet_id                   = aws_subnet.subnet1-public.id
 #   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
 #   associate_public_ip_address = true
@@ -113,7 +113,7 @@ resource "aws_instance" "worker-2" {
 #     ami = var.imagename
 #   availability_zone           = "us-east-1a"
 #   instance_type               = "t2.small"
-#   key_name                    = "newppk"
+#   key_name                    = "jenkey"
 #   subnet_id                   = aws_subnet.subnet1-public.id
 #   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
 #   associate_public_ip_address = true
@@ -130,7 +130,7 @@ resource "aws_instance" "worker-2" {
 #     ami = var.imagename
 #   availability_zone           = "us-east-1a"
 #   instance_type               = "t2.small"
-#   key_name                    = "newppk"
+#   key_name                    = "jenkey"
 #   subnet_id                   = aws_subnet.subnet1-public.id
 #   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
 #   associate_public_ip_address = true
